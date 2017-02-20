6158a10ba1596eaaec9abc75ff327489d4a31954
King James <King James>
2017-02-20 13:29:51 -0500

    completed user.js and modified index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
 var router = require('./router');
+var mongoose = require('mongoose');
+
+//DB Connection
+mongoose.connect('mongodb://localhost:auth/mongotest');
 
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true
+	},
+	password: String
+});
+
+var model = mongoose.model('user', userSchema);
+
+module.exports = model;
