1f0690c0571dd4097c32d36ec8538e531377ee8d
King James <King James>
2017-02-23 11:13:54 -0500

    completed module 15

--- client/src/actions/main.js
+++ client/src/actions/main.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 // const ROOT_URL = 'http://rest.learncode.academy/api/paul';
 const ROOT_URL = 'http://localhost:3000';
@@ -13,8 +14,13 @@ export function createPost(props) {
 }
 
 export function signinUser({ email, password }){
-
 	return function(dispatch){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
+			.then(response => {
+				browserHistory.push('/newitem');
+			})
+			.catch(() => {
+
+			});
 	}
 }
