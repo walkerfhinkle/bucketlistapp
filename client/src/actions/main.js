740526a60a1b670c6d5ab40e6997d4d83aa508a8
King James <King James>
2017-02-23 11:34:44 -0500

    module 16

--- client/src/actions/main.js
+++ client/src/actions/main.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER } from './types';
+import authReducer from '../reducers/auth_reducer';
 
 // const ROOT_URL = 'http://rest.learncode.academy/api/paul';
-const ROOT_URL = 'http://localhost:3000';
 export const CREATE_POSTS = 'CREATE_POSTS';
+const ROOT_URL = 'http://localhost:3000';
+
+export function signinUser({ email, password }){
+	return function(dispatch){
+		axios.post(`${ROOT_URL}/signin`, {email, password})
+			.then(response => {
+				dispatch({ type: AUTH_USER });
+
+				localStorage.setItem('token', response.data.token);
+				browserHistory.push('/newitem');
+			})
+			.catch(() => {
+
+			});
+	}
+}
 
 export function createPost(props) {
 	const request = axios.post(`${ROOT_URL}/posts`, props);
@@ -13,14 +30,3 @@ export function createPost(props) {
 	};
 }
 
-export function signinUser({ email, password }){
-	return function(dispatch){
-		axios.post(`${ROOT_URL}/signin`, {email, password})
-			.then(response => {
-				browserHistory.push('/newitem');
-			})
-			.catch(() => {
-
-			});
-	}
-}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import BandsReducer from './reducer_bands';
 import { reducer as formReducer } from 'redux-form';
+import {
+	AUTH_USER,
+	UNAUTH_USER
+} from '../actions/types';
 
-	const rootReducer = combineReducers({
-		form: formReducer
-	});
+const rootReducer = combineReducers({
+	form: formReducer
+});
 
-	export default rootReducer;
+export default rootReducer;
